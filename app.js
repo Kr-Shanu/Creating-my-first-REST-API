efda788dd5937692b507009d31719dc5b78d6bfb
Kumar Shanu <kumar.shanu2020@vitbhopal.ac.in>
2022-01-19 20:04:39 +0530

    Able to perform all operation with my REST api and has been tested on postman

--- app.js
+++ app.js
@@ -17,8 +17,6 @@ app.use(express.static("public"));
 
 
 
-
-
 /********************Setting up mongoose dataserver, schema, model and collection */
 
 // Connecting to mongo db server at local host 27017 with the database name wikiDB
@@ -42,46 +40,114 @@ const Article = mongoose.model("Article", articleSchema);
 // managing different verbs targeting same route **********************************/
 app.route("/articles")
 
-.get(function (req, res) {
-    Article.find(function (err, foundArticles) {
-        if (!err) {
-            res.send(foundArticles);
-        }
-        else {
-            res.send(err);
-        }
-    });
-})
+    .get(function (req, res) {
+        Article.find(function (err, foundArticles) {
+            if (!err) {
+                res.send(foundArticles);
+            }
+            else {
+                res.send(err);
+            }
+        });
+    })
 
-.post(function (req, res) {
-    // saving the new article data into the database using the article schema
-    const newArticle = new Article({
-        title: req.body.title,
-        content: req.body.content
-    });
-    // We can even semd a call back function in save functions which will help
-    // us know about any error occured.
-    newArticle.save(function (err) {
-        if (!err) {
-            res.send("Successfully added a new article!");
-        }
-        else {
-            res.send(err);
-        }
-    });
-})
+    .post(function (req, res) {
+        // saving the new article data into the database using the article schema
+        const newArticle = new Article({
+            title: req.body.title,
+            content: req.body.content
+        });
+        // We can even semd a call back function in save functions which will help
+        // us know about any error occured.
+        newArticle.save(function (err) {
+            if (!err) {
+                res.send("Successfully added a new article!");
+            }
+            else {
+                res.send(err);
+            }
+        });
+    })
 
-.delete(function (req, res) {
-    Article.deleteMany(function (err) {
-        if (!err) {
-            res.send("Successfully deleted all articles!");
-        }
-        else {
-            res.send(err);
-        }
+    .delete(function (req, res) {
+        Article.deleteMany(function (err) {
+            if (!err) {
+                res.send("Successfully deleted all articles!");
+            }
+            else {
+                res.send(err);
+            }
+        });
     });
-});
+
+
+
+
+// *************requesting to get a specific article*******************
+app.route("/articles/:articleTitle")
+
+    .get(function (req, res) {
+
+        Article.findOne({ title: req.params.articleTitle }, function (err, foundArticle) {
+            if (foundArticle) {
+                res.send(foundArticle);
+            }
+            else {
+                res.send("No articles maching those article is found");
+            }
+        });
+    })
+
+    // In order to prevent the overwrite the 4th line is added below.
+    .put(function (req, res) {
+
+        Article.updateOne(
+            { title: req.params.articleTitle },
+            { title: req.body.title, content: req.body.content },
+            function (err) {
+                if (!err) {
+                    res.send("Successfully updated the data");
+                }
+            });
+    })
+
+    .patch(function (req, res) {
+
+        Article.updateMany(
+            {title: req.params.articleTitle},
+            {title: req.body.title, content: req.body.content},
+                function(err) {
+                    if (!err) {
+                        res.send("Successfully updated article title to " + req.body.title);
+                    }
+                    else {
+                        res.send(err);
+                    }
+                }
+        );
+
+    })
+
+    .delete(function(req, res){
+
+        Article.deleteOne(
+            {title: req.params.articleTitle},
+            function(err){
+                if(!err)
+                {
+                    res.send("Successfully deleted the item which contained the title "+req.params.articleTitle);
+                }
+                else
+                {
+                    res.send(err);
+                }
+            }
+        );
+    });
+
 /***********************************************************************************/
+
+
 
 app.listen(process.env.PORRT || 3000, function () {
     console.log("App is running at local host 3000");
